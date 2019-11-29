6ebac16802fba00d9465590f540522cc2e2415f0
Teng Li <teng.li@yitu-inc.com>
2019-11-29 11:31:40 +0800

    init proj

---
+++ distributer/src/main/java/com/touter/distributer/DistributerApplication.java
@@ -0,0 +1,13 @@
+package com.touter.distributer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributerApplication.class, args);
+    }
+
+}
---
+++ distributer/src/test/java/com/touter/distributer/DistributerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.touter.distributer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
